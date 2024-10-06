8df1319222ed27f2169ee399e9c5da3a0676e15d
letalandroid <carlossoncra@gmail.com>
2024-10-06 09:52:13 -0500

    Creationg RecyclerView in process

---
+++ app/src/main/java/RV_RelojItem/Reloj_Adapter.java
@@ -0,0 +1,4 @@
+package RV_RelojItem;
+
+public class Reloj_Adapter {
+}
---
+++ app/src/main/java/RV_RelojItem/Reloj_ViewHolder.java
@@ -0,0 +1,23 @@
+package RV_RelojItem;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.lta.airlock.R;
+
+public class Reloj_ViewHolder extends RecyclerView.ViewHolder {
+
+    TextView lblNombreP;
+    TextView lblTipoP;
+    TextView lblPrecioP;
+
+    public Reloj_ViewHolder(@NonNull View itemView) {
+        super(itemView);
+        lblNombreP = itemView.findViewById(R.id.lblNombreP);
+        lblTipoP = itemView.findViewById(R.id.lblTipoP);
+        lblPrecioP = itemView.findViewById(R.id.lblPrecioP);
+    }
+}
