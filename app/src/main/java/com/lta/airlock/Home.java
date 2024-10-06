d1d9211751335039d598ca9a99cdc89073007806
letalandroid <carlossoncra@gmail.com>
2024-10-05 19:55:41 -0500

    Add MenuDown

--- app/src/main/java/com/lta/airlock/Home.java
+++ app/src/main/java/com/lta/airlock/Home.java
@@ -2,17 +2,46 @@ package com.lta.airlock;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.lta.airlock.databinding.ActivityMainBinding;
 
 public class Home extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    BottomNavigationView btnNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_home);
+
+        btnNav = findViewById(R.id.bottomNavigationView);
+
+        btnNav.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.elegant) {
+                replaceFragment(new FrElegant());
+            } else if (itemId == R.id.casual) {
+                replaceFragment(new FrCasual());
+            } else if (itemId == R.id.sports) {
+                replaceFragment(new FrSports());
+            }
+
+            return true;
+        });
+
+    }
+
+    private void replaceFragment(Fragment f) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.main, f);
+        ft.commit();
     }
 }
